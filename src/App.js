57a465b9c836cfc7384ac70518d28d7a2b38148c
Tomas Roy <tomas.roy2019@gmail.com>
2020-06-25 02:48:08 +0900

    5th commit

--- src/App.js
+++ src/App.js
@@ -112,5 +112,5 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default App
-//export default withAuthenticator(App)
+//export default App
+export default withAuthenticator(App)
